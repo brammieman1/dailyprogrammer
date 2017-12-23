d3036529a301bb66b27e20fd118401ecea05b8e6
Bram <a.j.denkers@student.utwente.nl>
2017-12-23 22:56:04 +0100

    completed challenge 1

---
+++ src/easy/Easy_001_Hacking.java
@@ -0,0 +1,34 @@
+package easy;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Easy_001_Hacking {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your Name?");
+        String name = scanner.nextLine();
+        System.out.println("What is your age?");
+        int age = Integer.parseInt(scanner.nextLine().trim());
+        System.out.println("What is your reddit username?");
+        String redditname = scanner.nextLine();
+        scanner.close();
+        System.out.println("Your name is " + name + " , you are " + age + "years old " + " and your username is " + redditname);
+
+    }
+
+    public static void logging(String name, int age, String reddit){
+        PrintStream output = null;
+        try {
+            output = new PrintStream(new File("data.txt"));
+            output.printf("%s\n%d\n%s", name, age, reddit);
+            output.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
